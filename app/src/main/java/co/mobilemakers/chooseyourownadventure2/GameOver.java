b52de2261ba515c66b4327a446067acd08f38787
david.burgos <david.burgos@globant.com>
2015-01-30 17:25:45 -0500

    --personalize the win or lose message

--- app/src/main/java/co/mobilemakers/chooseyourownadventure2/GameOver.java
+++ app/src/main/java/co/mobilemakers/chooseyourownadventure2/GameOver.java
@@ -1,7 +1,9 @@
 package co.mobilemakers.chooseyourownadventure2;
 
 import android.app.Fragment;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,6 +13,11 @@ import java.util.Random;
 
 public class GameOver extends Fragment {
 
+    private final static String USERNAME_PREFERENCE = "username_preference";
+    private final static String WIN_MESSAGE_PREFERENCE = "message_win_preference";
+    private final static String LOSE_MESSAGE_PREFERENCE = "message_lose_preference";
+
+    String mUsername, mWin_message, mLose_message;
 
     public GameOver() {
         // Required empty public constructor
@@ -22,7 +29,11 @@ public class GameOver extends Fragment {
 
         View RootView = inflater.inflate(R.layout.activity_game_over,container,false);
         Random random = new Random();
-        String parameter = (random.nextInt(30)<10?getString(R.string.title_win):getString(R.string.title_lose));
+
+        displayUserInfo();
+
+        String parameter = (random.nextInt(30)<10? mWin_message : mLose_message);
+        parameter = mUsername +" "+parameter;
 
         TextView textViewBody = (TextView)RootView.findViewById(R.id.txtMessage);
         textViewBody.setText(parameter);
@@ -31,4 +42,10 @@ public class GameOver extends Fragment {
     }
 
 
+    private void displayUserInfo(){
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        mUsername = sharedPreferences.getString(USERNAME_PREFERENCE,getString(R.string.default_username));
+        mWin_message = sharedPreferences.getString(WIN_MESSAGE_PREFERENCE,getString(R.string.title_win));
+        mLose_message = sharedPreferences.getString(LOSE_MESSAGE_PREFERENCE,getString(R.string.title_lose));
+    }
 }
